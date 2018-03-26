b98959c955c86123431461fb60cce2a992610300
MohammadYasar <yasar.samin@gmail.com>
2018-03-26 18:24:00 -0400

    Add files via upload

--- analyze_seg.py
+++ analyze_seg.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn import decomposition, mixture, preprocessing
-
+from sklearn import decomposition, mixture, preprocessing, externals
 summary_traj = []
 
 def loadData(vidFile = None, kinFile = None, transFile = None):
@@ -28,8 +27,12 @@ def extractCart(kinData):
         for j in range(3):
             new_s[i][j] = value[j+38]
             new_s[i][j+3] = value[j+57]
-    new_s = preprocessing.StandardScaler().fit(new_s).transform(new_s)
+    scaler = preprocessing.MinMaxScaler().fit(new_s)
+    #externals.joblib.dump(scaler, 'scaler.p')
+    #scaler = externals.joblib.load('scaler.p')
 
+    new_s = scaler.transform(new_s)
+    #new_s = preprocessing.normalize(new_s)
     mean, std, max, min = np.mean(new_s), np.std(new_s), np.amax(new_s), np.amin(new_s)
     #print "global mean: {} global std: {} global max: {} global min : {}".format(mean,std, max, min)
     return new_s
@@ -46,24 +49,33 @@ def labelProcess(newtrans):
 
     return trans
 
-def makeSegmentations(kinematics, transcripts):
+def makeSegmentations(kinematics, transcripts,task):
     transcripts = np.sort(transcripts, axis = 0)
     seg_kinematics = []
     prev_seg = transcripts[0][transcripts.shape[1]-1]
     for i in range(transcripts.shape[0]):
         if transcripts[i][transcripts.shape[1]-1] != prev_seg:
-            print prev_seg
-            processSegments(np.array(seg_kinematics), prev_seg)
+            processSegments(np.array(seg_kinematics), prev_seg, task)
             prev_seg = transcripts[i][transcripts.shape[1]-1]
             seg_kinematics = []
         for n in range (int(transcripts[i][0]), int(transcripts[i][1])):
             seg_kinematics.append(kinematics[n])
-    processSegments(np.array(seg_kinematics), prev_seg)
-    
-def processSegments(kinData, prev_seg):
+    processSegments(np.array(seg_kinematics), prev_seg, task)
+
+def processSegments(kinData, prev_seg, task):
     global summary_traj
-    mean, std, max, min = np.mean(kinData), np.std(kinData), np.amax(kinData), np.amin(kinData)
-    summary_traj.append([prev_seg,mean,std, max, min])
+    gesture = 'segmented_trajectories/{}G{}.p'.format(task,prev_seg)
+    externals.joblib.dump(kinData, gesture)
+    print kinData.shape
+    _mean, _std, _max, _min = [], [], [], []
+    _mean = np.mean(kinData, axis=0)
+    _std = np.std(kinData, axis=0)
+    _max = np.amax(kinData, axis = 0)
+    _min = np.amin(kinData, axis = 0)
+    temp_array = np.concatenate((np.concatenate((_mean, _std), axis = 0), np.concatenate((_max, _min), axis = 0)), axis = 0)
+    temp_array = np.append(temp_array,prev_seg)
+
+    summary_traj.append(temp_array)
     #print "prev_seg : {}, mean: {} std: {} max: {} min : {}".format(prev_seg,mean,std, max, min)
 
 def loopFiles(root):
@@ -76,19 +88,22 @@ def loopFiles(root):
             videoFile = root + 'Suturing/video/Suturing_{}00{}.avi'.format(alpha[i], j)
             print taskFile
             trans, kin = loadData(vidFile = videoFile, kinFile = taskFile, transFile = transcriptFile)
+            task = taskFile.replace('.txt','').split('/')[8]
             if len(trans)>0 and len(kin)>0:
-                makeSegmentations(kin, trans)
+                makeSegmentations(kin, trans, task)
     doProcessing()
+
 
 def doProcessing():
     global summary_traj
-    writeFile = '/Users/mohammadsaminyasar/Downloads/JIGSAWS/Suturing/kinematics/AllGestures/summary.csv'
+    writeFile = '/home/uva-dsa1/Downloads/dVRK videos/Suturing/kinematics/AllGestures/summary.csv'
 
-    print summary_traj
-    cp = pd.DataFrame(data = np.array(summary_traj))
+
+    cp = pd.DataFrame(data = np.array(summary_traj), columns = ['mean_x1', 'mean_y1', 'mean_z1', 'mean_x2', 'mean_y2', 'mean_z2', 'std_x1', 'std_y1', 'std_z1', 'std_x2', 'std_y2', 'std_z2', 'max_x1', 'max_y1', 'max_z1', 'max_x2', 'max_y2', 'max_z2', 'min_x1', 'min_y1', 'min_z1', 'min_x2', 'min_y2', 'min_z2', 'label'])
     cp.to_csv(writeFile, sep = ',')
 def main():
-    root = '/Users/mohammadsaminyasar/Downloads/JIGSAWS/'
+    root = '/home/uva-dsa1/Downloads/dVRK videos/'
+
     loopFiles(root)
 if __name__ == '__main__':
     main()
